4d34b8cf684cb78ea79d638e287575bb821552dc
Diego Cazes <dcazesv@gmail.com>
2018-11-04 21:21:53 -0300

    Primer commit

---
+++ Dominio/IngredienteReceta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    public class IngredienteReceta
+    {
+        private Ingrediente ingrediente;
+        private int cantidad;
+    }
+}
---
+++ Dominio/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    public class Usuario
+    {
+        List<RecetaConFecha> historialRecetas;
+        List<RecetaConFecha> recetasFavoritas;
+    }
+}
