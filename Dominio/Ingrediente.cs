476fa2b1c3d998fa087163d47a89d29061ab7996
Diego Cazes <dcazesv@gmail.com>
2018-11-06 21:19:32 -0300

    Se borro clase innecesaria, cambios en Ingrediente y pasoReceta

--- Dominio/Ingrediente.cs
+++ Dominio/Ingrediente.cs
@@ -15,14 +15,53 @@ namespace Dominio
         //1 = mg, 2 = ml
         public enum TipoMedida { Mg = 1, Ml = 2 }
 
-
-
+        public int _Id { set; get; }
+        public string _Nombre { set; get; }
+        public int _Costo { set; get; }
+        public TipoMedida _Medida { set; get; }
+        public int _MedidaPromedio { set; get; }
+        public int _MedidaPorGramo { set; get; }
+        public int _CantCaloriasPorMedida { set; get; }
+        public bool _AptoCeliacos { set; get; }
+        public bool _AptoDiabeticos{ set; get; }
+        public bool _AptoVegetarianos { set; get; }
+        public bool _AptoVeganos { set; get; }
         public Estacion _Estacion { set; get; }
         public Tipo _Tipo { set; get; }
 
-        public Ingrediente(int idTipo, int idEstacion) {
+        public Ingrediente(int id, string nombre, int costo, int tipoMedida, int medidaProm, int medidaGramo, int caloriasMedida, 
+            bool aptoCel, bool aptoDia, bool aptoVegetariano, bool aptoVegano, int idTipo, int idEstacion) {
+            _Id = id;
+            _Nombre = nombre;
+            _Costo = costo;
+            _Medida = (TipoMedida) tipoMedida;
             _Estacion = (Estacion) idEstacion;
             _Tipo = (Tipo) idTipo;
+            _MedidaPromedio = medidaProm;
+            _MedidaPorGramo = medidaGramo;
+            _CantCaloriasPorMedida = caloriasMedida;
+            _AptoCeliacos = aptoCel;
+            _AptoDiabeticos = aptoDia;
+            _AptoVegetarianos = aptoVegetariano;
+            _AptoVeganos = _AptoVeganos;
         }
+
+        public Ingrediente(string nombre, int costo, int tipoMedida, int medidaProm, int medidaGramo, int caloriasMedida,
+            bool aptoCel, bool aptoDia, bool aptoVegetariano, bool aptoVegano, int idTipo, int idEstacion){
+            _Nombre = nombre;
+            _Costo = costo;
+            _Medida = (TipoMedida)tipoMedida;
+            _Estacion = (Estacion)idEstacion;
+            _Tipo = (Tipo)idTipo;
+            _MedidaPromedio = medidaProm;
+            _MedidaPorGramo = medidaGramo;
+            _CantCaloriasPorMedida = caloriasMedida;
+            _AptoCeliacos = aptoCel;
+            _AptoDiabeticos = aptoDia;
+            _AptoVegetarianos = aptoVegetariano;
+            _AptoVeganos = _AptoVeganos;
+        }
+
+
     }
 }
--- Dominio/PasoReceta.cs
+++ Dominio/PasoReceta.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Drawing;
 namespace Dominio
 {
     public class PasoReceta
     {
+        public int _Id { set; get; }
+        public string _Texto { set; get; }
+        public int _TiempoReloj { set; get; }
+        public string _UrlVideo { set; get; }
+        public System.Drawing.Image
     }
 }
